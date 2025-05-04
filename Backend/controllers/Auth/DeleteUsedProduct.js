27990c0bbe5a65aef8d9ac32b4717674f516cd79
Dev Raj <dr395108@gmail.com>
2025-05-04 23:10:36 +0530

    added functionality to delete from multer folder

--- Backend/controllers/Auth/DeleteUsedProduct.js
+++ Backend/controllers/Auth/DeleteUsedProduct.js
@@ -1,4 +1,7 @@
 const product = require("../../Models/UsedProduct")
+const fs = require("fs")
+const path = require("path")
+
 const DeleteUsedProduct = async (req,res,next)=>{
       try{
             const {id} = req.body
@@ -8,6 +11,14 @@ const DeleteUsedProduct = async (req,res,next)=>{
                         success:false,
                         messgae:"Product ID not found"
                   })
+            }
+            for (let img of existingproduct.images){
+                  const filename = img.url
+                  const imgpath = path.join(__dirname,"../../Middleware/",filename)
+                  if(fs.existsSync(imgpath)){
+                       fs.unlinkSync(imgpath)
+                       console.log("File deleted")
+                  }
             }
             return res.status(200).json({
                         success:true,
